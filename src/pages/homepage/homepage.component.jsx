0d0fa156eec074d15720d0a3da0f87ed2bbd3baa
Gaurav Pandey <gaurav071@gmail.com>
2021-03-12 19:57:32 +0530

    initial commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default HomePage;
