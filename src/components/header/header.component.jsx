1fbb1081a938163ddd3ca598c44f06d4dd36ba7b
Gaurav Pandey <gaurav071@gmail.com>
2021-03-14 15:11:15 +0530

    Option Link styled component as div

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,6 @@ import {
   HeaderContainer,
   LogoContainer,
   OptionsContainer,
-  OptionDiv,
   OptionLink,
 } from "./header.styles";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
@@ -25,7 +24,9 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </OptionLink>
       ) : (
         <OptionLink to="/signin">SIGN IN</OptionLink>
       )}
